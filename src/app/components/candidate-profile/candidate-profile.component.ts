17361d12c954eec35c66ec6e98743661cacf2b92
mahmoud <mahhmoudrawagba@gmail.com>
2022-08-16 22:32:18 +0300

    Candidate Profile newPage

--- src/app/components/candidate-profile/candidate-profile.component.ts
+++ src/app/components/candidate-profile/candidate-profile.component.ts
@@ -1,6 +1,10 @@
+import { profileI } from './../../profile';
+import { CandidateService } from 'src/app/service/candidate.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, NgZone, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-candidate-profile',
@@ -8,12 +12,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./candidate-profile.component.css']
 })
 export class CandidateProfileComponent implements OnInit {
+  ProfileDetails: profileI;
 
-  constructor(private ngZone: NgZone, private http: HttpClient,
-    private router: Router) { }
+  constructor(private ngZone: NgZone,
+     private http: HttpClient,
+    private router: Router,
+    private fb: FormBuilder,
+    private cand: CandidateService,
+    ) { }
+
+
 
   ngOnInit(): void {
   }
+
+
 
   showFiller = false;
 
---
+++ src/app/profile.ts
@@ -0,0 +1,14 @@
+export interface profileI{
+    userId: number;
+    userName: string;
+    email: string;
+    fullName: string;
+    phone: string;
+    gender: string;
+    academicDegree: string;
+    field: string;
+    Intrest:string;
+    city:string;
+    dateOfBirth: string;
+    description: string;
+}
